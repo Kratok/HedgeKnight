ab71cd4ff389a1f3cdec735bfb267b9f9df86b57
kourpa <mlemus01@unm.edu>
2014-08-03 15:30:50 +0000

    Initial commit

---
+++ desktop/src/com/hedgeknight/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hedgeknight.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hedgeknight.game.HedgeKnight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HedgeKnight(), config);
+	}
+}
